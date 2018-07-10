12840eddda0d9544fe34309bff2bccbd57fd581a
iamsss <saurav9760@gmail.com>
2018-07-10 21:45:44 +0530

    Registering And Activating Sw

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,12 @@
 ﻿// Write your JavaScript code.
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(function () {
+        console.log('Service worker registered!');
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
